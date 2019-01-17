327fbe6347558c10b3ebb6e6fe0fa461780b4517
Sasha Khamkov <sanusart@gmail.com>
2019-01-17 22:58:49 +0200

    FIX (styles): fix site base css

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Header from '../components/header';
-import Footer from '../components/footer';
 import Demo from '../pages/demo';
 import Navigation from '../components/navigation';
 import styled from 'styled-components';
