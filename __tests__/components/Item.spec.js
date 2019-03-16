2062c83c2737ae53ddf50dd0ddca7a9cae572aa8
Sasha Khamkov <sanusart@gmail.com>
2019-03-16 18:15:51 +0200

    CHORE (update packages + tests): update packages and add more tests

--- __tests__/components/Item.spec.js
+++ __tests__/components/Item.spec.js
@@ -20,43 +20,54 @@ const props = (props = {}) => ({
 });
 
 describe('<Item/> component', () => {
-
   beforeEach(() => {
     spy = jest.fn();
   });
 
   test('renders correctly', () => {
-    const tree = TestRenderer.create(<Item {...props({ item: options[0] })}/>).toJSON();
+    const tree = TestRenderer.create(<Item {...props({ item: options[0] })} />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   test('onChange with click', () => {
-    TestRenderer.create(<Item {...props({ item: options[0] })} onClick={spy}/>)
-      .root.findByType('span').props.onClick();
+    TestRenderer.create(<Item {...props({ item: options[0] })} onClick={spy} />)
+      .root.findByType('span')
+      .props.onClick();
 
     expect(spy).toHaveBeenCalled;
   });
 
   test('onChange with key press', () => {
-    TestRenderer.create(<Item {...props({ item: options[0] })} ononKeyPress={spy}/>)
-      .root.findByType('span').props.onKeyPress();
+    TestRenderer.create(<Item {...props({ item: options[0] })} ononKeyPress={spy} />)
+      .root.findByType('span')
+      .props.onKeyPress();
 
     expect(spy).toHaveBeenCalled;
   });
 
   test('keepSelectedInList: false', () => {
     const tree = TestRenderer.create(
-      <Item {...props({ item: options[0], parentProps: {
-          itemRenderer: null,
-          keepSelectedInList: false
-        }, parentMethods: {
-          isSelected: () => true
-        } })}/>
+      <Item
+        {...props({
+          item: options[0],
+          parentProps: {
+            itemRenderer: null,
+            keepSelectedInList: false
+          },
+          parentMethods: {
+            isSelected: () => true
+          }
+        })}
+      />
+    ).toJSON();
+  });
+
+  test('pass item renderer', () => {
+    const tree = TestRenderer.create(
+      <Item {...props({ item: options[0], itemRenderer: () => <div>item</div> })} />
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
-
-
 });
