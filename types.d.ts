7db21d65c71840f9efc9f78c3099103eab37cd58
Sasha Khamkov <sanusart@gmail.com>
2019-08-21 22:39:42 +0300

    FIX (types): add correct types, fixes #26

--- types.d.ts
+++ types.d.ts
@@ -1,91 +1,99 @@
 import React from 'react';
 
 export interface IState {
-  dropdown: boolean;
-  values: object;
-  search: string;
-  selectBounds: object;
-  cursor: null;
+	dropdown: boolean;
+	values: object;
+	search: string;
+	selectBounds: object;
+	cursor: object;
 }
 
 export interface IMethods {
-  removeItem: () => void;
-  dropDown: () => void;
-  addItem: () => void;
-  setSearch: () => void;
-  getInputSize: () => void;
-  toggleSelectAll: () => void;
-  clearAll: () => void;
-  selectAll: () => void;
-  searchResults: () => void;
-  getSelectRef: () => void;
-  isSelected: () => void;
-  getSelectBounds: () => void;
-  areAllSelected: () => void;
-  handleKeyDown: () => void;
-  activeCursorItem: () => void;
-  createNew: () => void;
-  sortBy: () => void;
-  safeString: () => void;
+	removeItem: () => void;
+	dropDown: () => void;
+	addItem: () => void;
+	setSearch: () => void;
+	getInputSize: () => void;
+	toggleSelectAll: () => void;
+	clearAll: () => void;
+	selectAll: () => void;
+	searchResults: () => void;
+	getSelectRef: () => void;
+	isSelected: () => void;
+	getSelectBounds: () => void;
+	areAllSelected: () => void;
+	handleKeyDown: () => void;
+	activeCursorItem: () => void;
+	createNew: () => void;
+	sortBy: () => void;
+	safeString: () => void;
+}
+
+export interface IRenderer {
+	props?: ISelectProps;
+	state?: IState;
+	methods?: IMethods;
+}
+
+export interface IItemRenderer {
+	item?: number;
+	itemIndex?: number;
+	props?: ISelectProps;
+	state?: IState;
+	methods?: IMethods;
 }
 
 export interface ISelectProps {
-  addPlaceholder: '';
-  placeholder: 'Select...';
-  values: [];
-  options: [];
-  multi: false;
-  disabled: false;
-  searchBy: 'label';
-  sortBy: null;
-  clearable: false;
-  searchable: true;
-  dropdownHandle: true;
-  separator: false;
-  keepOpen: undefined;
-  noDataLabel: 'No data';
-  createNewLabel: 'add {search}';
-  disabledLabel: 'disabled';
-  dropdownGap: 5;
-  closeOnScroll: false;
-  debounceDelay: 0;
-  labelField: 'label';
-  valueField: 'value';
-  color: '#0074D9';
-  keepSelectedInList: true;
-  closeOnSelect: false;
-  clearOnBlur: true;
-  clearOnSelect: true;
-  dropdownPosition: 'bottom';
-  dropdownHeight: '300px';
-  autoFocus: false;
-  portal: null;
-  create: false;
-  direction: 'ltr';
-  name: null;
-  onChange: () => void;
-  onDropdownOpen: () => void;
-  onDropdownClose: () => void;
-  onClearAll: () => void;
-  onSelectAll: () => void;
-  onCreateNew: () => void;
-  searchFn: () => void;
-  clearRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
-  contentRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
-  dropdownRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
-  dropdownHandleRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
-  inputRenderer: ({ props: ISelectProps, state: IState, methods: IMethods, inputRef: any }) => void;
-  itemRenderer: ({
-    item: number,
-    itemIndex: number,
-    props: ISelectProps,
-    state: IState,
-    methods: IMethods
-  }) => void;
-  loadingRenderer: ({ props: ISelectProps }) => void;
-  noDataRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
-  optionRenderer: ({ item, props: ISelectProps, state: IState, methods: IMethods }) => void;
-  separatorRenderer: ({ props: ISelectProps, state: IState, methods: IMethods }) => void;
+	addPlaceholder?: string;
+	placeholder?: string;
+	values: object[];
+	options: object[];
+	multi?: boolean;
+	disabled?: boolean;
+	searchBy?: string;
+	sortBy?: string;
+	clearable?: boolean;
+	searchable?: boolean;
+	dropdownHandle?: boolean;
+	separator?: boolean;
+	keepOpen?: undefined;
+	noDataLabel?: string;
+	createNewLabel?: string;
+	disabledLabel?: string;
+	dropdownGap?: number;
+	closeOnScroll?: boolean;
+	debounceDelay?: number;
+	labelField?: string;
+	valueField?: string;
+	color?: string;
+	keepSelectedInList?: boolean;
+	closeOnSelect?: boolean;
+	clearOnBlur?: boolean;
+	clearOnSelect?: boolean;
+	dropdownPosition?: string;
+	dropdownHeight?: string;
+	autoFocus?: boolean;
+	portal?: string;
+	create?: boolean;
+	direction?: 'ltr' | 'rtl';
+	name?: null;
+	onChange: (value: any) => void;
+	onDropdownOpen?: () => void;
+	onDropdownClose?: () => void;
+	onClearAll?: () => void;
+	onSelectAll?: () => void;
+	onCreateNew?: () => void;
+	searchFn?: () => void;
+	clearRenderer?: ({ props, state, methods }: IRenderer) => void;
+	contentRenderer?: ({ props, state, methods }: IRenderer) => void;
+	dropdownRenderer?: ({ props, state, methods }: IRenderer) => void;
+	dropdownHandleRenderer?: ({ props, state, methods }: IRenderer) => void;
+	inputRenderer?: ({ props, state, methods }: IRenderer) => void;
+	itemRenderer?: ({ item, itemIndex, props, state, methods }: IItemRenderer) => void;
+	loadingRenderer?: ({ props }: IItemRenderer) => void;
+	noDataRenderer?: ({ props, state, methods }: IRenderer) => void;
+	optionRenderer?: ({ item, props, state, methods }: IItemRenderer) => void;
+	separatorRenderer?: ({ props, state, methods }: IRenderer) => void;
 }
 
 declare const Select: React.SFC<ISelectProps>;
