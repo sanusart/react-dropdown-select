0cd3f5003dc749e89151248c43d24b2bfe30c2c1
Sasha Khamkov <sanusart@gmail.com>
2026-07-25 15:12:40 +0300

    refactor(release cycle): release cycle

--- change-log-builder.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-COMMITS=$(git log `git describe --tags --abbrev=0`..HEAD --oneline --pretty=format:'*  %s [View](https://github.com/sanusart/react-dropdown-select/commit/%H)' --reverse | grep -v Merge)
-
-printf "### vPLACEHOLDER\n${COMMITS}\n\n" | cat - CHANGELOG.md > /tmp/temp-msg && mv /tmp/temp-msg CHANGELOG.md
-
---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+import config from '@commitlint/config-conventional';
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        ...(config.rules['type-enum']?.[2] || []),
+        'typo',
+        'i18n',
+      ],
+    ],
+  },
+};
