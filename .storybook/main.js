85a50ea4eba95188a88d422c64a7185bd38516e6
Sasha Khamkov <sanusart@gmail.com>
2026-07-25 23:57:37 +0300

    chore: fix docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,10 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ['./stories/*.mdx', './stories/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-webpack5-compiler-swc'],
+  stories: ['./stories/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-webpack5-compiler-swc'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-    defaultName: 'Documentation',
   },
 };
 export default config;
